eafb2b9808e8bc3d581e0111058d652bf7811139
Archizavr <artemij.radionov@gmail.com>
2025-07-07 14:42:48 +0300

    Added React client

---
+++ web_app_client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import NavigationBar from './components/Navigation.jsx'
+
+function App() {
+
+  return (
+    <>
+      <NavigationBar />
+    </>
+  )
+}
+
+export default App
